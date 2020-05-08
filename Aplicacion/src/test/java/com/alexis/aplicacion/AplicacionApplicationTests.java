283135d05714d6e94e04a38ad950e7e0a77b4e88
Alexis <Alexis@DESKTOP-9H8FTUS>
2020-05-08 17:05:20 -0500

    Comit inicial con Eclipse. Close #1

---
+++ Aplicacion/src/test/java/com/alexis/aplicacion/AplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexis.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
